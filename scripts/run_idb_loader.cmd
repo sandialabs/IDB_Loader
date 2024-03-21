a21cb3a3a84d147209de2a9b2e7589e5de0ba3b9
srschwa <163152952+srschwa@users.noreply.github.com>
2024-03-21 12:36:36 -0600

    Update run_idb_loader.cmd
    change jar name to match jar's name

--- scripts/run_idb_loader.cmd
+++ scripts/run_idb_loader.cmd
@@ -1,5 +1,5 @@
 @Title IDB Loader
 
-java -Dlog4j2.configurationFile=config/log4j2.xml -jar idb-1.0.0-SNAPSHOT.jar config/lanl_mox_mass_percent_loader.properties
+java -Dlog4j2.configurationFile=config/log4j2.xml -jar idb_loader-1.0.0-SNAPSHOT.jar config/lanl_mox_mass_percent_loader.properties
 
 @PAUSE
